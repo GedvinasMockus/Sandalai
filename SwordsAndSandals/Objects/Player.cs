795eb14709b445c2690b5c28990ba88bad8701ec
Povilas Paškevičius <po.paskevicius@ktu.edu>
2023-09-16 20:33:26 +0300

    Implemented ability rendering

--- SwordsAndSandals/Objects/Player.cs
+++ SwordsAndSandals/Objects/Player.cs
@@ -1,50 +1,61 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace SwordsAndSandals.Objects
 {
-    public class Player : IDrawable
+    public class Player
     {
-        public Texture2D texture { get; }
         public Vector2 position { get; set; }
 
+        private Texture2D texture;
         private int frameWidth;
         private int frameHeight;
         private float scale;
+        private Dictionary<string, Ability> abilities;
 
 
-        public Player(Texture2D texture, Vector2 position, float scale)
+        public Player(Texture2D texture, Vector2 position, float scale, Dictionary<string, Ability> availableAbilities)
         {
             this.texture = texture;
             this.position = position;
             frameWidth = texture.Width;
             frameHeight = texture.Height;
             this.scale = scale;
+            this.abilities = availableAbilities;
         }
 
-        public Player(Texture2D texture, Vector2 position, float scale, int width, int height)
+        public Player(Texture2D texture, Vector2 position, float scale, int width, int height, Dictionary<string, Ability> availableAbilities)
         {
             this.texture = texture;
             this.position = position;
             frameWidth = width;
             frameHeight = height;
             this.scale = scale;
+            this.abilities = availableAbilities;
         }
 
         public void Draw(SpriteBatch batch)
         {
-            //Matrix m = Matrix.CreateScale(scale) * Matrix.CreateTranslation((float)(-frameWidth / 2), (float)(-frameHeight / 2), 0) * Matrix.CreateTranslation(position.X, position.Y, 0);
-            //batch.Begin(transformMatrix: m);
-            //batch.Draw(texture, new Vector2(0, 0), Color.White);
-            //batch.End();
-            batch.Draw(texture, new Vector2(position.X, position.Y), new Rectangle(0, 0, frameWidth, frameHeight), Color.White, 0.0f, new Vector2(frameWidth / 2, frameHeight), scale, SpriteEffects.None, 1);
+            batch.Draw(texture, new Vector2(position.X, position.Y - scale * frameHeight/2), new Rectangle(0, 0, frameWidth, frameHeight), Color.White, 0.0f, new Vector2(frameWidth / 2, frameHeight / 2), scale, SpriteEffects.None, 1);
+
+            int index = 0;
+            foreach(var v in abilities.Values)
+            {
+                v.position = new Vector2(position.X + scale * (frameWidth/2 - frameWidth * (index & 1)), position.Y - v.texture.Height * v.scale * (index/2) - v.texture.Height/2 * v.scale);
+                v.Draw(batch);
+                index++;
+            }
         }
 
         public void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            foreach(var v in abilities.Values)
+            {
+                v.Update(gameTime);
+            }
         }
     }
 }
---
+++ SwordsAndSandals/Utils/JsonUtils.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SwordsAndSandals.Utils
+{
+    public static class JsonUtils
+    {
+        public static T ReadJson<T>(string filename)
+        {
+            using (Stream s = File.OpenRead(filename))
+            {
+                return JsonSerializer.Deserialize<T>(s);
+            }
+        }
+    }
+}
