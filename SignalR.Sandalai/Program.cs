34c1a43d87cc07421c123f22467a02968f8b42c6
ErnestasFil <100300400+ErnestasFil@users.noreply.github.com>
2023-09-17 03:10:01 +0300

    SignalR implementation

---
+++ SignalR.Sandalai/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Owin.Hosting;
+
+namespace SignalR.Sandalai
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://192.168.1.182:8081";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine($"Server running on {url}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
