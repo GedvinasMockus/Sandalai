91c0fed95de4ddfc71fa22152d84dc0c3bb3b5df
InVictus21 <gedvinas.mockus@gmail.com>
2023-10-18 00:41:19 +0300

    Completed Prototype Design Pattern

--- SignalR.Sandalai/Objects/Player.cs
+++ SignalR.Sandalai/Objects/Player.cs
@@ -1,19 +1,30 @@
 ﻿using System.Numerics;
+using System;
 
 namespace SignalR.Sandalai.Objects
 {
-    public class Player
+    public class Player : Prototype
     {
         public string ConnectionId { get; private set; }
 
         public string ClassName { get; private set; }
         public Vector2 Position { get; set; }
         public FlipEnum Flip { get; set; }
-
         public Player(string connId, string className)
         {
             ConnectionId = connId;
             ClassName = className;
+        }
+
+        public override Prototype Clone()
+        {
+            Player player = (Player)this.MemberwiseClone();
+            player.ConnectionId = String.Copy(ConnectionId);
+            player.ClassName = String.Copy(ClassName);
+            player.Position = new Vector2(this.Position.X, this.Position.Y);
+            player.Flip = this.Flip;
+
+            return player;
         }
         public PlayerInfo GetInfo()
         {
