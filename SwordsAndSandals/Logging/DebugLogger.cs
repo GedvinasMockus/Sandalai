a2e569fc88505c40f216325a4be86c6d11105bde
InVictus21 <gedvinas.mockus@gmail.com>
2023-12-06 21:05:08 +0200

    Added Chain of Responsibility

---
+++ SwordsAndSandals/Logging/DebugLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordsAndSandals.Logging
+{
+    public class DebugLogger : Logger
+    {
+        public DebugLogger(Logger nextLogger) : base(nextLogger)
+        {
+            base.nextLogger = nextLogger;
+        }
+
+        public override void process(Log Log)
+        {
+            if (Log.LogLevel == "DEBUG")
+            {
+                string timestamp = DateTime.Now.ToString("yyyy_MM_dd-HH:mm:ss: ");
+                timestamp = timestamp.Split("-")[1];
+                string message = timestamp + "[DEBUG] " + Log.Message;
+                Debug.WriteLine(message);
+            }
+            else
+            {
+                nextLogger.process(Log);
+            }
+        }
+    }
+}
---
+++ SwordsAndSandals/Logging/InfoLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordsAndSandals.Logging
+{
+    public class InfoLogger : Logger
+    {
+        public InfoLogger(Logger nextLogger) : base(nextLogger)
+        {
+            base.nextLogger = nextLogger;
+        }
+
+        public override void process(Log Log)
+        {
+            if(Log.LogLevel == "INFO")
+            {
+                string timestamp = DateTime.Now.ToString("yyyy_MM_dd-HH:mm:ss: ");
+                string fileName = "Logs/" + timestamp.Split("-")[0] + "_Logs.txt";
+                timestamp = timestamp.Split("-")[1];
+                string message = timestamp + "[INFO] " + Log.Message;
+                string currentDirectory = Directory.GetCurrentDirectory();
+                string filePath = Path.Combine(currentDirectory, fileName);
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+            else
+            {
+                nextLogger.process(Log);
+            }
+        }
+    }
+}
